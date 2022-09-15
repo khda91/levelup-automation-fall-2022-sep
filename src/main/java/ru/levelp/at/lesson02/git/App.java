905ed5020a6b1c7bc532cb7fd862dba2ca2e326a
Khodakovskiy Dmitriy <khda91@gmail.com>
2022-09-15 19:45:17 +0300

    added calculator app

---
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -0,0 +1,14 @@
+package ru.levelp.at.lesson02.git;
+
+public class App {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Сумма");
+
+        System.out.println("2 + 2 = " + calculator.sum(2, 2));
+
+        System.out.println("========");
+        System.out.println();
+    }
+}
