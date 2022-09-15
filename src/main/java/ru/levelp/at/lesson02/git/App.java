65cea7195ddce02295528e4ef7b868f8a0767bb2
Khodakovskiy Dmitriy <khda91@gmail.com>
2022-09-15 20:49:37 +0300

    added sum, subtract, multiply examples

--- src/main/java/ru/levelp/at/lesson02/git/App.java
+++ src/main/java/ru/levelp/at/lesson02/git/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.println("Сумма");
 
         System.out.println("2 + 2 = " + calculator.sum(2, 2));
+        System.out.println("100 + 100 = " + calculator.sum(100, 100));
 
         System.out.println("========");
         System.out.println();
@@ -14,6 +15,7 @@ public class App {
         System.out.println("Вычитание");
 
         System.out.println("2 - 2 = " + calculator.subtract(2, 2));
+        System.out.println("3 - 2 = " + calculator.subtract(3, 2));
 
         System.out.println("========");
         System.out.println();
@@ -21,6 +23,7 @@ public class App {
         System.out.println("Умножение");
 
         System.out.println("2 * 2 = " + calculator.multiply(2, 2));
+        System.out.println("4 * 4 = " + calculator.multiply(4, 4));
 
         System.out.println("========");
         System.out.println();
