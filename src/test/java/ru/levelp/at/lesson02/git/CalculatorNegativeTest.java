cd15ab927e9187875a375628f84fce7f7deee6eb
Khodakovskiy Dmitriy <khda91@gmail.com>
2022-10-17 20:41:23 +0300

    Lesson 11 - CI/CD.
    - gitlab-ci
    - added additional jobs
    - script

--- src/test/java/ru/levelp/at/lesson02/git/CalculatorNegativeTest.java
+++ src/test/java/ru/levelp/at/lesson02/git/CalculatorNegativeTest.java
@@ -1,11 +1,10 @@
 package ru.levelp.at.lesson02.git;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CalculatorNegativeTest {
 
