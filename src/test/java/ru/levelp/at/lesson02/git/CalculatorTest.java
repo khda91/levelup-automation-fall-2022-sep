71c4d98d120cfca130936a64ddcf49c328c56ecc
Khodakovskiy Dmitriy <khda91@gmail.com>
2022-10-17 20:29:20 +0300

    Lesson 11 - CI/CD.
    - gitlab-ci
    - added additional jobs

--- src/test/java/ru/levelp/at/lesson02/git/CalculatorTest.java
+++ src/test/java/ru/levelp/at/lesson02/git/CalculatorTest.java
@@ -1,10 +1,10 @@
 package ru.levelp.at.lesson02.git;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
